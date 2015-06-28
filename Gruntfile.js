105a1dad99e3f71c28645a5da1334a613dcce5c1
Daniel Beckwith <djbsnx@gmail.com>
2015-06-28 16:04:45 -0400

    Fixed src files for grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
         tasks: ['jshint:gruntfile']
       },
       src: {
-        files: ['<%= src_dir %>', '<%= test_dir %>'],
+        files: ['<%= src_dir %>/**/*', '<%= test_dir %>/**/*'],
         tasks: ['test']
       }
     },
